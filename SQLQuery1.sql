3f7ee91b7566826d128128d4f76639079c57a20d
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-05-14 21:40:05 +0530

    initial commit

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,77 @@
- CREATE DATABASE PAYROLL_SERVICE;
+ ---------------UC1---------------
+ 
+ CREATE DATABASE PAYROLL_SERVICE; 
  USE PAYROLL_SERVICE;
+ 
+ ----------UC2------------
+-----Cerating employee payroll table-------
+ CREATE TABLE EMPLOYEE_PAYROLL
+ (
+ Id int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+ NAME VARCHAR(100) NOT NULL,
+ Salary FLOAT NOT NULL,
+ StartDate DATE NOT NULL,
+
+);
+ SELECT * FROM EMPLOYEE_PAYROLL;
+
+
+ -------UC3----------
+-------Inserting values into Employee_payroll table----------
+
+INSERT INTO employee_payroll VALUES('Himanshu','50000','12-july-2020');
+INSERT INTO employee_payroll VALUES('Vishal','40000','12-jan-2010');
+INSERT INTO employee_payroll VALUES('Harshpal','15000','20-mar-2018');
+INSERT INTO employee_payroll VALUES('Krunal','10000','15-FEB-2010');
+INSERT INTO employee_payroll VALUES('Kawasaki','5','13-apr-2025');
+
+---------UC4-------------
+------------Retrieving all the Tables value--------------
+SELECT * FROM EMPLOYEE_PAYROLL;
+
+-----------------UC5---------------------
+ ------retrieve salary data for a particular employee------
+ select Salary from employee_payroll where Name='Kawasaki';
+
+ select * from employee_payroll where StartDate between cast('1-apr-2019' as date) and cast(getdate() as date);
+
+ ----------------------UC6------------------
+------Adding GENDER column to the employee_payroll table using ALTER keyword-------
+
+ALTER TABLE employee_payroll ADD Gender CHAR(1);
+
+------------Updating Gender using UPDATE keyword--------------
+UPDATE employee_payroll SET Gender='M' WHERE Name='Himanshu' or Name= 'Kawasaki' or Name='Harshpal' or Name='Vishal'or Name= 'Krunal';
+
+-----------------------UC7----------------------
+-----------------Finding SUM,AVG,MIN,MAX,COUNT-----------------
+SELECT SUM(Salary) as SumOfSalary FROM employee_payroll;
+SELECT SUM(Salary) as SumOfSalary FROM employee_payroll WHERE Gender='M' GROUP BY Gender;
+
+SELECT AVG(Salary) as Average FROM employee_payroll WHERE Gender='M' GROUP BY Gender;
+
+SELECT MIN(Salary) as Minimum FROM employee_payroll WHERE Gender='M' GROUP BY Gender;
+
+SELECT MAX(Salary) as Maximum FROM employee_payroll;
+
+SELECT COUNT(Id) as Number_of_Emp FROM employee_payroll WHERE Gender='M' GROUP BY Gender;
+
+---------------------UC8--------------------
+-----Extending employee_payroll table-----------
+
+ALTER TABLE employee_payroll ADD Phone_Number varchar(20);
+
+------Adding column with DEFAULT value-----
+ALTER TABLE employee_payroll ADD Emp_Address varchar(20) DEFAULT 'INDIA';
+
+ALTER TABLE employee_payroll ADD Emp_DEPT varchar(10)DEFAULT 'HR';
+
+INSERT INTO employee_payroll VALUES('Dolly','25000','23-aug-2019','F','9833678738','Nainital','CS');
+INSERT INTO employee_payroll VALUES('Tarun','25000','23-feb-2019','M','9833678538',DEFAULT,DEFAULT);
+
+-----------------UC9----------------
+------Extending employee_payroll table-------
+
+ALTER TABLE employee_payroll ADD Basic_Pay varchar(10), Deduction FLOAT(20), Taxable_Pay FLOAT(20), Income_Tax Float(20), Net_Pay float(20);
+
+
